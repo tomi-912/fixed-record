687676eac602500700eed5e675c2829a20aa5efe
tomi-912 <tomi-912@users.noreply.github.com>
2026-08-11 06:14:16 +0000

    deploy: da9805e6fdae3e796181fe39d8d6fe79034f303b

---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["fixed_record",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"fixed_record/io/struct.Writer.html\" title=\"struct fixed_record::io::Writer\">Writer</a>&lt;W&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[433]}
