433513cc258c7a7cfbfd5dbb63bef03d0af8c9c7
tomi-912 <tomi-912@users.noreply.github.com>
2026-08-22 12:10:20 +0000

    deploy: c607aee731e6ff49f75c6c5d272e4bd3c7e0248f

--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["fixed_record",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"fixed_record/io/struct.Writer.html\" title=\"struct fixed_record::io::Writer\">Writer</a>&lt;W&gt;",0]]]]);
+    const implementors = Object.fromEntries([["fixed_record",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"fixed_record/io/struct.Writer.html\" title=\"struct fixed_record::io::Writer\">Writer</a>&lt;W&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
